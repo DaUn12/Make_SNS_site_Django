3058442706c31282bb48961d7c49c4670a9f04c1
Jeong Da Un <wjekdns019@likelion.org>
2021-07-02 11:45:28 +0900

    7/2 COMMIT

---
+++ homepa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for homepa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homepa.settings')
+
+application = get_asgi_application()
---
+++ popo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from popo.views import hello_world
+
+app_name = 'popo'
+
+# urlpatterns = [
+#     path('hello_world/', hello_world, name='hello_world')
+# ]
+
+# urlpatterns = [
+#     path('hi/', hi, name='hi')
+# ]
