e50505c06512c85b1ff5b750e9a531e61ce703a4
Jeong Da Un <wjekdns019@likelion.org>
2021-07-02 12:14:15 +0900

    7/2 COMMIT22

--- popo/views.py
+++ popo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('Hello wordl!!')
+    return render(request,'bbase.html')
 
 
 # def hi(request):
