05a1079c75eb177d9f32cd1169e085f82b0daa17
Jeong Da Un <wjekdns019@likelion.org>
2021-08-06 11:26:55 +0900

    8/6
     COMMIT2233

--- articleapp/decorators.py
+++ articleapp/decorators.py
@@ -4,12 +4,12 @@ from articleapp.models import Article
 
 
 def article_ownership_required(func):
-    def decorated(requet, *args, **kwargs):
+    def decorated(request, *args, **kwargs):
         target_article = Article.objects.get(pk=kwargs['pk'])
-        if target_article.writer == requet.user:
-            return func(requet, *args, **kwargs)
+        if target_article.writer == request.user:
+            return func(request, *args, **kwargs)
 
         else:
             return HttpResponseForbidden()
 
-    return decorated()
+    return decorated
