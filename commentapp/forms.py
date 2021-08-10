59fc23afd75b3258d5c659eacc521ac18dad4f8a
Jeong Da Un <wjekdns019@likelion.org>
2021-08-10 12:07:09 +0900

    8/10
     COMMIT2233

--- commentapp/forms.py
+++ commentapp/forms.py
@@ -3,8 +3,11 @@ from django.forms import ModelForm
 from commentapp.models import Comment
 
 
-class CommnetCreationForm(ModelForm):
+class CommentCreationForm(ModelForm):
     class Meta:
+        # meta : 장고에서 지원하는 플랫폼,
+        #      : 이미지의 픽셀이 실제데이터인데 외부적인 정보를 정리하는것
+        #      : meta 가 있어야 밑에있는 정보를 보여줄 수도 있음
         model = Comment
         fields = ['content']
 
