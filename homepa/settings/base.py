40292b96d6a58ce62d1ce89f7491bc6d0a7d9a9c
Jeong Da Un <wjekdns019@likelion.org>
2021-09-14 11:19:50 +0900

    9/14  -1
     COMMIT2233

--- homepa/settings/base.py
+++ homepa/settings/base.py
@@ -15,32 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent       # 'Path(__fill__).resolve : settings.py 의 경로 // parent : 파일의 부모노드
-
-env_list =dict()
-local_env = open(os.path.join(BASE_DIR, '.env'), encoding='utf-8')      #os.path.join : os의 경로(settings의 파일과 .env 파일의 경로)를 합침
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n','')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z!wute_#a%5cmq6lu^6=33e^)=6zq)cuv3l_vasd4ja!2ow288'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]       # 모든 호스트를 허용
-
+# BASE_DIR 메인앱 폴더의 경로를 의미
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# 'Path(__fill__).resolve : base.py 의 경로 // parent : 파일의 부모노드
 
 # Application definition
 
@@ -98,15 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'homepa.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
